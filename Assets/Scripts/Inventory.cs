25dd3ede88c6b9c437b370c3336b5bac6766f27b
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2022-01-25 10:47:52 +0100

    Inventory(pt4)
    Modified 'Inventory' script. Now picked item shows in inventory (but still doesn't disappear from the scene <-- it have to be fixed)

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject[] items = new GameObject[10];
+    [SerializeField] GameObject[] inventoryPos = new GameObject[10];
+    // [SerializeField] Sprite[] inventoryPos = new Sprite[10];
+
+    public Sprite spr;
+
+    public int count = 0;
+
     void Start()
     {
         
@@ -13,6 +21,22 @@ public class Inventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        settingInventory();
+    }
+
+    public void settingInventory(){
+        for(int i = 0; i < count; i++){
+            spr = items[i].GetComponent<Image>().sprite;
+            inventoryPos[i].GetComponent<Image>().sprite = spr;
+            
+        }
+    }
+
+    public void fillInventory(GameObject itemToInsert){
+        count++;
+        for(int i = count; i >0; i--){
+            items[i] = items[i-1];  
+        }
+        items[0] = itemToInsert;
     }
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Inventory invScript;
+    
+
     void Start()
     {
-
+        invScript = GameObject.Find("/Inventory/Inventory").GetComponent<Inventory>();
         
     }
 
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D col){
-        Destroy(gameObject);
+        invScript.fillInventory(gameObject);
+        // Destroy(gameObject);
+        // gameObject.transform.position.x = -1000;
     } 
 }
