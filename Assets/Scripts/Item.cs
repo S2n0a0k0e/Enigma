b93e867942344acde75e07f8badcc3a815543b4d
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2022-01-28 14:29:01 +0100

    Items
    Items 'dissapears' after being picked.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class Item : MonoBehaviour
 {
     public Inventory invScript;
+    public Rigidbody2D rb;
     
 
     void Start()
@@ -18,6 +19,7 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D col){
         invScript.fillInventory(gameObject);
+        transform.position = new Vector2(0,0);
         // Destroy(gameObject);
         // gameObject.transform.position.x = -1000;
     } 
