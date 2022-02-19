2e0a55e0c763e7bca2f26103e5fa54dc720d89c2
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2022-02-19 01:35:43 +0100

    Winning animation
    Added animation for player after paired all oxydes.

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerAnimations : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject player;
+    [SerializeField] GameObject targetGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(targetGameObject.GetComponent<GameSettings>().toWin == 0)
+        {
+            animator.SetBool("allOxydes", true);
+        }
+    }
+
+    void DestroyPlayer(){
+        Destroy(player);
+    }
+}
