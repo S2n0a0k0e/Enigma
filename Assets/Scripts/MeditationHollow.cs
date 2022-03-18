6c34bb2311248e995b8cc2eccce33c3bf9a63de3
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2022-03-18 23:49:51 +0100

    Meditation Hollows
    Made physics for meditation hollows.

---
+++ Assets/Scripts/MeditationHollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MeditationHollow : MonoBehaviour
+{
+    float plX;
+    float plY;
+    float hollowX;
+    float hollowY;
+
+    float distanceX;
+    float distanceY;
+    float distance;
+    void OnTriggerStay2D(Collider2D col){
+        plX = col.gameObject.transform.position.x;
+        plY = col.gameObject.transform.position.y;
+        hollowX = this.gameObject.transform.position.x;
+        hollowY = this.gameObject.transform.position.y;
+        distanceX = plX - hollowX;
+        distanceY = plY - hollowY;
+        Player plScript = col.gameObject.GetComponent<Player>();
+        distance = Mathf.Sqrt(distanceX*distanceX + distanceY*distanceY);
+        if(distance <= 0.4f){
+            plScript.rb.velocity -= new Vector2(distanceX*3f, distanceY*3f);
+    
+        }
+    }
+}
