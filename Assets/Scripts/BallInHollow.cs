86bc766308cb467d6a16a0e2d0849f53c69b5ffe
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2022-03-20 23:56:06 +0100

    BallInHollow script
    Added script responsible for detecting if a marble is in hollow.

---
+++ Assets/Scripts/BallInHollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallInHollow : MonoBehaviour
+{
+    public bool inHole = false;
+    public bool stillInHole = true;
+    public GameSettings gameScript;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+            if(inHole && !stillInHole)
+        {
+            gameScript.holesToWin--;    
+            stillInHole = true;
+            Debug.Log("ok");
+        }
+        // else if(inHole && stillInHole)
+        // {
+        //     gameScript.holesToWin++; 
+        // }
+    }
+    
+}
--- Assets/Scripts/MeditationHollow.cs
+++ Assets/Scripts/MeditationHollow.cs
@@ -14,17 +14,25 @@ public class MeditationHollow : MonoBehaviour
     float distanceY;
     float distance;
     void OnTriggerStay2D(Collider2D col){
-        plX = col.gameObject.transform.position.x;
-        plY = col.gameObject.transform.position.y;
-        hollowX = this.gameObject.transform.position.x;
-        hollowY = this.gameObject.transform.position.y;
-        distanceX = plX - hollowX;
-        distanceY = plY - hollowY;
-        Player plScript = col.gameObject.GetComponent<Player>();
-        distance = Mathf.Sqrt(distanceX*distanceX + distanceY*distanceY);
-        if(distance <= 0.4f){
-            plScript.rb.velocity -= new Vector2(distanceX*3f, distanceY*3f);
-    
-        }
+        
+            plX = col.gameObject.transform.position.x;
+            plY = col.gameObject.transform.position.y;
+            hollowX = this.gameObject.transform.position.x;
+            hollowY = this.gameObject.transform.position.y;
+            distanceX = plX - hollowX;
+            distanceY = plY - hollowY;
+            Player plScript = col.gameObject.GetComponent<Player>();
+            distance = Mathf.Sqrt(distanceX*distanceX + distanceY*distanceY);
+            if(distance <= 0.4f){
+                plScript.rb.velocity -= new Vector2(distanceX*3f, distanceY*3f);
+        
+            }
+            // Debug.Log(distance);
+            if(distance <= 0.2f){
+                col.gameObject.GetComponent<BallInHollow>().inHole = true;
+            }
+        
     }
+
+
 }
