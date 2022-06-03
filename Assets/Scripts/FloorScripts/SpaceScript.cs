aeae6d3623c3ec1dbef2d76a83bbe84f31cce2e0
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2022-06-03 21:21:10 +0200

    Space
    Added space floor prefab

---
+++ Assets/Scripts/FloorScripts/SpaceScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceScript : MonoBehaviour
+{
+    [SerializeField] Sprite space1;
+    [SerializeField] Sprite space2;
+    [SerializeField] Sprite space3;
+    [SerializeField] Sprite space4;
+    [SerializeField] Sprite space5;
+    [SerializeField] Sprite space6;
+    Sprite actualTexture;
+    Image m_Image;
+
+    void Start()
+    {
+        m_Image = GetComponent<Image>();
+       SetRandomTexture ();
+       m_Image.sprite = actualTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetRandomTexture ()
+    {
+        int index = Random.Range(0,6);
+        switch (index)
+        {
+            case 0:
+                actualTexture = space1;
+                break;
+            case 1:
+                actualTexture = space2;
+                break;
+            case 2:
+                actualTexture = space3;
+                break;
+            case 3:
+                actualTexture = space4;
+                break;
+            case 4:
+                actualTexture = space5;
+                break;
+            case 5:
+                actualTexture = space6;
+                break;
+        }
+    }
+    
+}
