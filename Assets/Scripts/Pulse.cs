cd8ac67de7bffa3eca63ebdbf5a703ea1d8a1468
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2022-09-22 00:52:30 +0200

    Pulse interaction
    Pulse blocks change the velocity of the player.

--- Assets/Scripts/Pulse.cs
+++ Assets/Scripts/Pulse.cs
@@ -9,9 +9,44 @@ public class Pulse : MonoBehaviour
     
     void OnCollisionEnter2D(Collision2D col)
     {
+        Rigidbody2D rb = col.gameObject.GetComponent<Rigidbody2D>();
         if(!animator.GetBool("isHit") && col.gameObject.tag != "UnmovableBlock"){ 
             animator.SetBool("isHit", true); 
         }
+
+        
+        if( rb.velocity.x < 0)
+        {
+            rb.velocity = rb.velocity + new Vector2(-4f, 0f);
+        }
+        if( rb.velocity.x > 0)
+        {
+            rb.velocity = rb.velocity + new Vector2(4f, 0f);
+        }
+        if( rb.velocity.y < 0)
+        {
+            rb.velocity = rb.velocity + new Vector2(0f, -4f);
+        }
+        if( rb.velocity.y > 0)
+        {
+            rb.velocity = rb.velocity + new Vector2(0f, 4f);
+        }
+        // if(Input.GetAxis("Mouse X")<0){
+        //     rb.velocity = rb.velocity + new Vector2((-mouseX1), 0f);
+            
+        // }
+        // if(Input.GetAxis("Mouse X")>0){
+        //     rb.velocity = rb.velocity + new Vector2(mouseX1, 0f);
+            
+        // }
+        // if(Input.GetAxis("Mouse Y")<0){
+        //     rb.velocity = rb.velocity + new Vector2(0f, (-mouseY1));
+            
+        // }
+        // if(Input.GetAxis("Mouse Y")>0){
+        //     rb.velocity = rb.velocity + new Vector2(0f, (mouseY1));
+            
+        // }
     }
 
     public void stopAnim(){
